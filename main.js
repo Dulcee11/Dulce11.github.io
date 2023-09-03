339a7e6ddc6e9146a3df5d95f462e2ccbfc36e8a
var name = Dulce <91805186+Dulcee11@users.noreply.github.com>
2023-09-03 12:47:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Soy programadora web y lo aprendí en Tecnolochicas.')
+  .typeString('Soy Gestora e Instructora de Clubs de Tecnología para Niñas.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
